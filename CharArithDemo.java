d46edc1ad0ad74f35e1922c5f93f3f049daa0564
rdatu <r.datu@klab.com>
2015-06-05 17:17:18 +0800

    Characters - CharArith.java

---
+++ CharArithDemo.java
@@ -0,0 +1,9 @@
+class CharArithDemo{
+	public static void main (String args[]){
+		char ch;
+		ch = 'X';
+		System.out.println("ch contains " + ch);
+		ch = 90;
+		System.out.println("ch is now " + ch);
+	}
+}
