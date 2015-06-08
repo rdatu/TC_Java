ae4b042385b553ee074fc1777d01726f1e555e38
rdatu <r.datu@klab.com>
2015-06-08 16:38:26 +0800

    Irregular Array Example - Ragged.java

---
+++ Ragged.java
@@ -0,0 +1,31 @@
+class Ragged{
+	public static void main(String args[]){
+		int riders[] [] = new int [7] [];
+		riders[0] = new int[10];
+		riders[1] = new int[10];
+		riders[2] = new int[10];
+		riders[3] = new int[10];
+		riders[4] = new int[10];
+		riders[5] = new int[2];
+		riders[6] = new int[2];
+		
+		
+		int i, j;
+
+		for(i = 0; i < 5; i++)
+			for(j = 0; j < 10; j++)
+				riders[i][j] = i + j + 10;
+		for(i = 5; i< 7; i++)
+			for(j = 0; j < 2; j++)
+				riders[i][j] = 1 + j + 10;
+		
+		System.out.println("Riders per trip during the Week: ");
+		for (i=0;i<5;i++){
+			for(j = 0; j < 10; j++)
+				System.out.print(riders[i][j] + "\t");
+			System.out.println();
+		}
+		
+
+	}
+}
