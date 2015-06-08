3eb398809aef0db632626485df51015bee8a6e87
rdatu <r.datu@klab.com>
2015-06-08 09:48:23 +0800

    String Literal Examples and Backslash characters - StrDemo.java

---
+++ StrDemo.java
@@ -0,0 +1,7 @@
+class StrDemo{
+	public static void main(String args[]){
+		System.out.println("First line\nSecond Line"); //\n is used to generate a new line
+		System.out.println("A\tB\tC"); //\t is used to generate a tabbed space
+		System.out.println("D\tE\tF");	
+	}
+}
