f31f41b65e9a201ecabc5e5690d550f392827fa3
rdatu <r.datu@klab.com>
2015-06-08 10:49:27 +0800

    Type Conversions - LtoD.java

---
+++ LtoD.java
@@ -0,0 +1,14 @@
+class LtoD{
+	public static void main(String args[]){
+		long L;
+		double D;
+		L = 100123285L;
+		D = L;
+
+		System.out.println("L and D: " + L + " " + D);
+		
+		D = 100123285.0;
+		//L = D; // Illegal
+
+	}
+}
