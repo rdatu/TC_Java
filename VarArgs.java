3260e0507e49d3b2056c2afe40bf97c26882cebf
rdatu <r.datu@klab.com>
2015-06-09 11:14:06 +0800

    Nested Classes / Variable Argument Method

---
+++ VarArgs.java
@@ -0,0 +1,14 @@
+class VarArgs{
+	static void vaTest(int ... v){
+		System.out.println("Number of args: " + v.length);
+		System.out.println("Contents: ");
+		for(int i = 0;i<v.length; i++)
+			System.out.println(" arg " + i + ": " + v[i]);
+		System.out.println();
+	}
+	public static void main(String args[]){
+		vaTest(10);
+		vaTest(1,2,3,4,5,6,7);
+		vaTest();
+	}
+}
