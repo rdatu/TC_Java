7fb8e6e0e1723c0c24502617fa11c70eb0a305bd
rdatu <r.datu@klab.com>
2015-06-09 15:56:11 +0800

    Using Interface Reference

--- SeriesDemo.java
+++ SeriesDemo.java
@@ -1,19 +1,17 @@
 class SeriesDemo{
 	public static void main(String args[]){
-		ByTwos ob = new ByTwos();
-			
-		for(int i=0;i < 5; i++)
-			System.out.println("Next Value is " + ob.getNext());
+		ByTwos twoOb = new ByTwos();
+		ByThrees threeOb = new ByThrees();
+		Series ob;
 
-		System.out.println("\nResetting");
-		ob.reset();
-		for(int i = 0; i < 5; i++)
-			System.out.println("Next value is " + ob.getNext());
-		
-		System.out.println("\nStarting at 100");
-		ob.setStart(100);
-		
-		for(int i = 0;i < 5;i++)
-			System.out.println("Next value is " + ob.getNext());
+
+		for(int i=0; i<5 ; i++){
+			ob = twoOb;
+			System.out.println("Next ByTwos value is " + ob.getNext());
+			
+			ob = threeOb;
+			System.out.println("Next ByThrees value is " + ob.getNext());
+		}
+
 	}
 }
