033020411807f34419565cd6f87d88671b8e3dbb
rdatu <r.datu@klab.com>
2015-06-11 11:57:02 +0800

    Annotations and Metadata Examples

---
+++ AnnoDemo.java
@@ -0,0 +1,16 @@
+@Deprecated
+class MyClass{
+	private String msg;
+	MyClass (String m){
+		msg =m;
+	}
+	String getMsg(){
+		return msg;
+	}
+}
+class AnnoDemo{
+	public static void main(String args[]){
+		MyClass myObj = new MyClass("test");
+		System.out.println(myObj.getMsg());
+	}
+}
