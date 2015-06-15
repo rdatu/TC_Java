5a66c28bbc14a189dd03f4715dfeccc83a62d4ac
rdatu <r.datu@klab.com>
2015-06-15 10:40:33 +0800

    Applet Skeleton Demo

---
+++ Applet/src/AppletSkel.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.applet.*;
+public class AppletSkel {
+	public void init(){
+		//initialize the applet	
+	}
+	public void start(){
+		//start the applet
+	}
+	public void stop(){
+		//suspends execution
+	}
+	public void destroy(){ //last method executed, called when applet is terminated
+		//perform shutdown activity
+	}
+	public void paint(Graphics g){
+		//redisplay contents of a window
+	}
+
+}
